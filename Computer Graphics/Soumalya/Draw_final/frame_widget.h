82b126a99e4402fe30f975790e83bdbbbf04ba59
Soumalya1857 <soumalyakundu10@gmail.com>
2020-12-13 23:51:31 +0530

    CG added upto lines

---
+++ Computer Graphics/Soumalya/Draw_final/frame_widget.h
@@ -0,0 +1,63 @@
+#ifndef FRAME_WIDGET_H
+#define FRAME_WIDGET_H
+
+#include <QObject>
+#include <QWidget>
+#include <QMouseEvent>
+#include <QEvent>
+#include <QFrame>
+#include <QList>
+#include <QPoint>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QPair>
+class frame_widget : public QFrame
+{
+    Q_OBJECT
+private:
+    int size;
+    bool grid;
+    bool modified;
+    int maxwidth;
+    int maxheight;
+    bool visibleAxes;
+    bool lineDDA;
+    bool lineBresh;
+    QList <QPair<QPoint, QColor> > points;
+    QPoint point1;
+    QPoint point2;
+    QPoint lastpoint;
+    QPoint convertPixel(QPoint p);
+    QColor currentcol;
+    QPoint convertCoord(int x, int y);
+
+
+
+
+public:
+    frame_widget(QWidget *parent = nullptr);
+
+    void createGrid();
+    void changeSize(int x);
+    void showAxes();
+    void changeGridSize(int x);
+    void hideAxes();
+    void toggleAxes();
+    void toggleGrid();
+    void mouseMoveEvent(QMouseEvent *ev) override;
+    void paintEvent(QPaintEvent *p) override;
+    void mousePressEvent(QMouseEvent *ev) override;
+    QPoint setPoint1();
+    QPoint setPoint2();
+    void changeCurrentColour(QColor q);
+    void drawLineDDA();
+    void drawLineBresh();
+signals:
+    void sendCoord(int x, int y);
+    void sendCoordForMousePress(int x, int y);
+    void sendTimeDDA(double);
+    void sendTimeBresh(double);
+};
+
+#endif // FRAME_WIDGET_H
